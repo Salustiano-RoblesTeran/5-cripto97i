f2563618013d6a1804b44a9316f9e5872022432c
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-08 00:05:13 -0300

    Creando seccion del portfolio

--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -1,37 +1,53 @@
-import React from 'react'
-import numeral from 'numeral'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 
-const TableCoins = ( {coins} ) => {
+const TableCoins = ({ coins }) => {
   return (
     <>
-    <table className="table table-hover table-coins">
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Symbol</th>
-          <th>Name</th>
-          <th>Price Usd</th>
-          <th>Change 24Hr</th>
-        </tr>
-      </thead>
-      <tbody>
-        {coins.map((coin)=> (
-                <tr key={coin.id}>
-                    <td>{coin.rank}</td>
-                    <td>
-                        <Link to={`/coin/${coin.id}`}><img src={`https://assets.coincap.io/assets/icons/${coin.symbol.toLowerCase()}@2x.png`} alt={coin.name} /></Link>
-                    </td>
-                    <td>{coin.name}</td>
-                    <td>{numeral(coin.priceUsd).format("$0,0.00")}</td>
-                    <td className= {coin.changePercent24Hr < 0 ? 'text-danger' : 'text-success'}>{parseFloat(coin.changePercent24Hr).toFixed(2)}%</td>
-                </tr>
-        ))}
+      <table className="table table-hover table-coins">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Price Usd</th>
+            <th>Change 24Hr</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>{coin.rank}</td>
+              <td>
+                <Link to={`/coin/${coin.id}`}>
+                  <img
+                    className="img-thumbnail"
+                    src={`https://assets.coincap.io/assets/icons/${coin.symbol.toLowerCase()}@2x.png`}
+                    alt={coin.name}
+                  />
+                </Link>
+              </td>
+              <td>{coin.name}</td>
+              <td>{numeral(coin.priceUsd).format('$0,0.00')}</td>
+              <td className={coin.changePercent24Hr < 0 ? 'text-danger' : 'text-success'}>
+                {parseFloat(coin.changePercent24Hr).toFixed(2)}%
+              </td>
+              <td>
+              <button
+              className="btn btn-outline-secondary"
+              type="button"
+              >
+                +
+        </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-      </tbody>
-    </table>
-  </>
-  )
-}
-
-export default TableCoins
+export default TableCoins;
